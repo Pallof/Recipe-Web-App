c2fc5bb24874b3f382916f67d78f8cc610a7f0ad
Daniel <dls556@nyu.edu>
2020-04-14 00:19:10 -0700

    milestone 2 is complete, need to get API KEY from google. Fix up HBS and CSS. Start to implement VUE for frontend. Do not try to filter yet, that is still a WIP

---
+++ config/passport-setup.js
@@ -0,0 +1,10 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+
+passport.use(new GoogleStrategy({
+    //option for the strategy
+
+    }), () => {
+        //passport callback function
+    }
+);
